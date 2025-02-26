47bdf103a48ea763de0b68a3e9cba9f099b92d60
Vaibhav Rajendra Sable <125455641+Vaibhavsable3@users.noreply.github.com>
2025-02-26 11:16:15 +0530

    Add files via upload
    The Java Default Constructor program is in DefaultConst.java file.

---
+++ DefaultConst.java
@@ -0,0 +1,16 @@
+// Java Program to demonstrate
+// Default Constructor
+class DefaultConst {
+
+    // Default Constructor
+     DefaultConst() {
+          System.out.println("Default constructor"); }
+
+    // Driver function
+    public static void main(String[] args)
+    {
+         DefaultConst obj1 = new  DefaultConst();
+          
+                
+    }
+}
